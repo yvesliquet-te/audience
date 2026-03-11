2a883f9c448fbfa167268211a6cbc8c74d54b346
yvesliquet-te <yves.liquet@gmail.com>
2026-03-11 09:52:53 +0100

    Change export to CommonJS module syntax

--- api/analyze.js
+++ api/analyze.js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-  // CORS headers
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -31,4 +30,4 @@ export default async function handler(req, res) {
   } catch (err) {
     res.status(500).json({ error: err.message || 'Erreur serveur' });
   }
-}
+};
